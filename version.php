e530c9a8bd28bad1778255bb1dbf0fdb304aa3ab
David OC <davidherzlos@gmail.com>
2025-03-20 19:57:28 -0600

    release v4.1.3

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'enrol_coursecompleted';
-$plugin->release = 'v4.1.2';
+$plugin->release = 'v4.1.3';
 $plugin->version = 2025013101;
